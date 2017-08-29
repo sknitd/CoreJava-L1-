d89e7e4a94e944fb16b8483a45301464e0063743
sknitd <sk39693@gmail.com>
2017-08-29 10:15:38 +0530

    Add files via upload

--- pushbackIS.java
+++ pushbackIS.java
@@ -5,26 +5,17 @@ public class pushbackIS {
       ByteArrayOutputStream bOutput = new ByteArrayOutputStream(100);
 
       while( bOutput.size()!= "This is Java Input/Output".length() ) {
-         // Gets the inputs from the user
+       
          bOutput.write("This is Java Input/Output".getBytes());  
       }
-      byte b [] = bOutput.toByteArray();
-      System.out.println("Print the content");
+      byte b [] = bOutput.toByteArray();// converts input to byte array
+     
       
-      for(int x = 0; x < b.length; x++) {
-         // printing the characters
-         System.out.print((char)b[x]); 
-      }
-      System.out.println("   ");
+     
 
-      int c;
+   
       ByteArrayInputStream bInput = new ByteArrayInputStream(b);
-     /* System.out.println("Converting characters to Upper case " );
-      
-      for(int y = 0 ; y < 1; y++ ) {
-         while(( c = bInput.read())!= -1) {
-            System.out.println(Character.toUpperCase((char)c));
-         }*/
+    
       int ctr=0;
       PushbackInputStream push = new PushbackInputStream(bInput);  
       int i;        
@@ -33,21 +24,14 @@ public class pushbackIS {
               if(ctr==1)
             	  {
             	  System.out.println((char)i);
-            	  push.unread(Character.toLowerCase((char)i));  
+            	  push.unread(Character.toLowerCase((char)i));  // converts first character to
+            	  //lower case
             	  }
         	  
-        	 /* if(i == '#') {  
-                  int j;  
-                  if( (j = push.read()) == '#'){  
-                       System.out.print("**");  
-                  }else {  
-                      push.unread(j);  
-                      System.out.print((char)i);  
-                  }  
-              }*///else {  
+        	 
               else
                           System.out.print((char)i);  
-              //}  
+              
          }        
          bInput.reset(); 
       }
