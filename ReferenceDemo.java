d5ce718f3b4b54c7062c2caf96b8917db35dfdce
sknitd <sk39693@gmail.com>
2017-09-06 11:34:45 +0530

    Add files via upload

---
+++ ReferenceDemo.java
@@ -0,0 +1,20 @@
+interface MyInterface{}
+class MyClass1 {
+}
+class MyClass2 extends MyClass1 implements MyInterface {
+}
+class MyClass3 implements MyInterface {
+}
+class ReferenceDemo{
+public static void main(String args[]){
+MyClass1 class1Object = new MyClass1 ();
+MyClass2 class2Object = new MyClass2 ();
+MyClass3 class3Object = new MyClass3();
+
+//class2Object = class3Object;
+//class3Object = class2Object;
+MyInterface InterfaceRef = class3Object;//this is a valid statement
+//class3Object = (MyClass3) class2Object;
+//class2Object = class1Object;
+}
+}
